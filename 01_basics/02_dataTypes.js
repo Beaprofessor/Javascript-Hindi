a7219d86a39e3fc49a98904d38433752f5759d28
Beaprofessor <130960371+Beaprofessor@users.noreply.github.com>
2023-12-16 09:22:44 +0000

    Datatypes...

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // Treat all the JS code as newer version. 
+
+
+// alert (3+3) // We are using NodeJS,  not the Browser
+
+console.log(3+
+    3)
+    // Code readability should be HIGH.
+
+console.log("Lokesh")
+
+let name = "Lokesh"
+let age = 19
+let isLoggedIn = false
+
+
+/* Primitive Dataypes */
+
+// number => 2 to power53 
+// bigint
+//string => " "
+//boolean => true/false
+// null => Standalone value
+// undefined => abhi value assign nhi hui hai 
+// symbol => uniqueness find krne ke liye use hota hai
+
+// Object
+
+console.log(typeof undefined) // Output - Undefined
+console.log(typeof null) // Output - Object
