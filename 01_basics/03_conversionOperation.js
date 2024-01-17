ab69764d8495a7ef51eaa7bbeb989fd6d0dbe368
Beaprofessor <130960371+Beaprofessor@users.noreply.github.com>
2024-01-17 16:30:16 +0000

    Operations Conversion Discussed.......

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,12 +1,12 @@
-let score = "33"
+// let score = "33"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 
-let valueInNumber  = Number(score);
-console.log(typeof(valueInNumber));
-console.log(valueInNumber);
+// let valueInNumber  = Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
@@ -14,10 +14,10 @@ console.log(valueInNumber);
 // null => 0
 // undefined => NaN
 
-let isLoggedIn = "Lokesh"
+// let isLoggedIn = "Lokesh"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 // 1 => true ; 0 => false.
 // "" => false
@@ -25,9 +25,49 @@ console.log(booleanIsLoggedIn);
 
 
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof (stringNumber));
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof (stringNumber));
 
+
+
+// ***********************Operations*******************
+
+let value = 3
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1 = "Hello";
+let str2 = " Hitesh";
+
+let str3 = str1 + str2 ; 
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);   // Do  not  write this type of code.... **BAD APPROACH**....
+// console.log(1 + 2 + "2");   // Do  not  write this type of code.... **BAD APPROACH**....
+
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");  // Do  not  write this type of code.... **BAD APPROACH**....
+
+let num1, num2 , num3;
+
+// num1 = num2 = num3 = 2+2  // Readability 0 kardi bhaiya aapne kya kar rhe ho yee ??/
+
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
