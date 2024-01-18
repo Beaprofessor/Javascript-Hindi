0cac86ffa8316ef06cecb0a7d1cb3b152f9a4e78
Beaprofessor <130960371+Beaprofessor@users.noreply.github.com>
2024-01-18 06:30:55 +0000

    Comparison Done...

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,25 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);  // make sure that ki aap same datatype ke variable ko compare kar rhe ho...
+
+
+// console.log(null > 0);  // Output - false
+// console.log(null == 0); //  Output - false
+// console.log(null >= 0); //  Output - true ..... // True isliye kyuki comparisons operator {>,<,>=,<=} ye differnetly work karte hai eqaulity operator se{==} , Comparison null ko 0 kar dete hai , isliye null >= 0 true hai and baaki ke ye false....
+
+
+// console.log(undefined > 0); // Output - false
+// console.log(undefined == 0);// Output - false
+// console.log(undefined < 0);// Output - false
+// Iss type ke conversions ko hamesha try to avoid....
+
+
+// strict check -> {===} ye datatype ko bhi check karta hai..
+
+console.log("2" === 2);
